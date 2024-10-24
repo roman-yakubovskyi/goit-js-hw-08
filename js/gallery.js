8bc07d5c7135d42e292a633580d8c3e8ba8357f0
roman-yakubovskyi <yakubovskyi133@gmail.com>
2024-10-24 03:27:52 +0200

    add js fixed10

--- js/gallery.js
+++ js/gallery.js
@@ -67,7 +67,8 @@ const images = [
 const galleryList = document.querySelector('.gallery');
 const galleryItems = images
   .map(({ preview, original, description }) => {
-    return `<li class="gallery-item">
+    return `
+    <li class="gallery-item">
       <a class="gallery-link" href="${original}">
         <img
           class="gallery-image"
